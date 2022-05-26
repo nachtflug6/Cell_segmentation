fcb7a12891bb08c248eda821c3cab6e7390d64ba
nachtflug6 <52052701+nachtflug6@users.noreply.github.com>
2022-05-26 15:19:19 -0400

    improved

--- models/unet_original.py
+++ models/unet_original.py
@@ -7,7 +7,10 @@ import numpy as np
 import torch as th
 import torch.nn as nn
 
-from .base_model import BaseModel
+from models.blocks.constant_blocks import *
+from models.blocks.reduction_blocks import *
+from models.blocks.unet_blocks import *
+from models.base_model import BaseModel
 
 
 class UNet(BaseModel):
@@ -23,51 +26,60 @@ class UNet(BaseModel):
 
         self.dropout = nn.Dropout(p=0.2)
 
-        self.cnn1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                  nn.ReLU(),
-                                  nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                  nn.ReLU())
+        self.cnn1 = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU())
 
-        self.cnn2 = nn.Sequential(nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                  nn.ReLU(),
-                                  nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                  nn.ReLU())
+        self.cnn2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU())
 
-        self.cnn3 = nn.Sequential(nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                  nn.ReLU(),
-                                  nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                  nn.ReLU())
+        self.cnn3 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU())
 
-        self.cnn4 = nn.Sequential(nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                  nn.ReLU(),
-                                  nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                  nn.ReLU())
+        self.cnn4 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU())
 
-        self.cnn5 = nn.Sequential(nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                  nn.ReLU(),
-                                  nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                  nn.ReLU())
+        self.cnn5 = nn.Sequential(
+            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU(),
+            nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU())
 
-        self.cnn1_up = nn.Sequential(nn.Conv2d(1024, 512, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                     nn.ReLU(),
-                                     nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                     nn.ReLU())
+        self.cnn1_up = nn.Sequential(
+            nn.Conv2d(1024, 512, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU())
 
-        self.cnn2_up = nn.Sequential(nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                     nn.ReLU(),
-                                     nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                     nn.ReLU())
+        self.cnn2_up = nn.Sequential(
+            nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU())
 
-        self.cnn3_up = nn.Sequential(nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                     nn.ReLU(),
-                                     nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                     nn.ReLU())
+        self.cnn3_up = nn.Sequential(
+            nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU())
 
-        self.cnn4_up = nn.Sequential(nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                     nn.ReLU(),
-                                     nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
-                                     nn.ReLU(),
-                                     nn.Conv2d(64, params['out_classes'], kernel_size=1, stride=1, padding=0))
+        self.cnn4_up = nn.Sequential(
+            nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU(),
+            nn.Conv2d(64, params['out_classes'], kernel_size=1, stride=1, padding=0))
 
         self.max_pool_2 = nn.MaxPool2d(2)
 
@@ -79,7 +91,6 @@ class UNet(BaseModel):
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, input_tensor):
-
         output_1 = self.cnn1(input_tensor)
         output_1 = self.dropout(output_1)
         output_1 = self.batch_1(output_1)
@@ -121,5 +132,106 @@ class UNet(BaseModel):
         return output
 
 
+class UNet2(BaseModel):
+    def __init__(self, params):
+        super(BaseModel, self).__init__()
+        self.depth = params['depth']
+        self.out_classes = params['out_classes']
+
+        init_layers = params['start_layers'] * params['dim_multiplier']
+        start_layers = params['start_layers']
+
+        self.input_layer = nn.Sequential(
+            nn.Conv2d(1, init_layers + start_layers, kernel_size=params['input_conv_kernel_size'],
+                      stride=1, padding=params['input_conv_kernel_size'] // 2,
+                      padding_mode=params['padding_mode']),
+            nn.ReLU()
+        )
 
 
+        self.start_layers = start_layers
+
+        down_blocks = []
+        up_blocks = []
+
+        for i in range(self.depth):
+            down_block = DownBlock(
+                nn.Sequential(
+                    # InceptionResNetModuleA(start_layers, start_layers, padding_mode=params['padding_mode']),
+                    nn.Conv2d(start_layers + init_layers, start_layers + init_layers, kernel_size=3, padding=1,
+                              padding_mode=params['padding_mode']),
+                    nn.ReLU(),
+                    nn.Dropout(params['dropout_p']),
+                    nn.BatchNorm2d(start_layers + init_layers)
+                ),
+                nn.Sequential(
+                    IncResReductionBy2ModuleA(start_layers + init_layers, (2 * start_layers) + init_layers, padding_mode=params['padding_mode'])))
+
+            up_block = UpBlock(
+                nn.Sequential(nn.ConvTranspose2d((start_layers * 2) + init_layers, start_layers + init_layers, 2, stride=2)),
+                nn.Sequential(nn.Conv2d(2 * start_layers + 2 * init_layers, start_layers + init_layers, kernel_size=1, stride=1, padding=0),
+
+                              nn.ReLU(),
+                              # InceptionResNetModuleA(start_layers, start_layers, padding_mode=params['padding_mode']),
+                              nn.Dropout(params['dropout_p']),
+                              nn.BatchNorm2d(start_layers + init_layers)
+                              ))
+
+            start_layers = 2 * start_layers
+
+            down_blocks.append(down_block)
+            up_blocks.append(up_block)
+
+        self.down_blocks = nn.ModuleList(down_blocks)
+        self.up_blocks = nn.ModuleList(up_blocks)
+
+        self.buttom_layer = nn.Sequential(
+            nn.Conv2d(start_layers + init_layers, start_layers + init_layers, kernel_size=3, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU(),
+            nn.Dropout(params['dropout_p']),
+            nn.BatchNorm2d(start_layers + init_layers),
+            nn.Conv2d(start_layers + init_layers, start_layers + init_layers, kernel_size=3, padding=1, padding_mode=params['padding_mode']),
+            nn.ReLU(),
+            # InceptionResNetModuleA(start_layers, start_layers, padding_mode=params['padding_mode']),
+            nn.Dropout(params['dropout_p']),
+            nn.BatchNorm2d(start_layers + init_layers)
+        )
+
+        self.output_layer1 = nn.Sequential(
+            nn.Conv2d(params['start_layers'] + init_layers, params['start_layers'], kernel_size=3, padding=1,
+                      padding_mode=params['padding_mode']),
+            nn.ReLU(),
+            # InceptionResNetModuleA(params['start_layers'], params['start_layers'], padding_mode=params['padding_mode']),
+            # nn.Dropout(params['dropout_p']),
+            # nn.BatchNorm2d(params['start_layers']),
+            # nn.Conv2d(params['start_layers'], params['out_classes'], kernel_size=1, stride=1, padding=0)
+        )
+
+        self.output_layer2 = nn.Sequential(
+            nn.Conv2d(params['start_layers'], params['out_classes'], kernel_size=1, stride=1, padding=0),
+            nn.Softmax(dim=1))
+
+    def forward(self, input_tensor):
+
+        current_output = self.input_layer(input_tensor)
+        first_output = current_output
+        # input_tensor = current_output
+        concat_outputs = []
+
+        for down_block in self.down_blocks:
+            concat_output, current_output = down_block.forward(current_output)
+            concat_outputs.append(concat_output)
+
+        current_output = self.buttom_layer.forward(current_output)
+
+        for i, up_block in enumerate(reversed(self.up_blocks)):
+            idx = self.depth - 1 - i
+            current_output = up_block.forward(current_output, concat_outputs[idx])
+
+        output = self.output_layer1(current_output)  # , dim=1))
+
+        # output = output + input_tensor.expand(-1, self.start_layers, -1, -1)
+
+        output = self.output_layer2(output)
+
+        return output
