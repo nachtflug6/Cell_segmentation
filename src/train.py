8340cc2659b117efe4fa19088fde5b2323c7c31b
nachtflug6 <52052701+nachtflug6@users.noreply.github.com>
2022-08-31 20:10:27 -0400

    cluster test2

--- src/train.py
+++ src/train.py
@@ -1,1 +1,11 @@
-print('Hello World!')
+import pandas as pd
+import os
+import numpy as np
+
+
+cwd = os.getcwd()
+out_path = os.path.join(cwd, '../results')
+df = pd.DataFrame()
+df['test'] = np.random.randint(0, 10, 10)
+print(df['test'])
+df.to_csv(os.path.join(out_path, 'test.csv'), index=False)
