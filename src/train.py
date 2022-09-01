f360175c2acd9e691bdf9f8d77de104a858848fb
nachtflug6 <52052701+nachtflug6@users.noreply.github.com>
2022-08-31 21:02:19 -0400

    nextnext try

--- src/train.py
+++ src/train.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 cwd = os.getcwd()
-out_path = os.path.join(cwd, '../results')
+out_path = os.path.join(cwd, '../to50jego/Cell_segmentation/results')
 df = pd.DataFrame()
 df['test'] = np.random.randint(0, 10, 10)
 print(df['test'])
